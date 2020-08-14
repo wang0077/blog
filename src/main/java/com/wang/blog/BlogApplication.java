acec05f6f2d9050b88e502e93d8dc58f1008bf24
wAnG <228913299@qq.com>
2020-08-14 08:34:55 +0800

    test

--- src/main/java/com/wang/blog/BlogApplication.java
+++ src/main/java/com/wang/blog/BlogApplication.java
@@ -3,9 +3,11 @@ package com.wang.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @MapperScan(value = "com.wang.blog.dao")
 @SpringBootApplication
+//@ComponentScan(value = "com.wang.blog")
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
