84292e24ed71db2df81990f1c370fa9ef7bc2e4a
wAnG <228913299@qq.com>
2020-10-21 17:08:08 +0800

    添加Controller的注释

--- src/main/java/com/wang/blog/Controller/admin/TypeController.java
+++ src/main/java/com/wang/blog/Controller/admin/TypeController.java
@@ -10,32 +10,65 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
+/**
+ * @author wangsiyuan
+ */
 @Controller
 @RequestMapping("/admin/types")
 public class TypeController {
 
-    @Autowired
+
     private ITypeService service;
 
+    /**
+     * 操作的回调信息储存在session的名字
+     */
+    private static final String OPERATION = "operation";
+
+    /**
+     * 更新操作
+     */
+    private static final String UPDATE = "update";
+
+    /**
+     * 删除操作
+     */
+    private static final String DELETE = "delete";
+
+    /**
+     * 新增错误的回调信息
+     */
+    private static final String MSG = "msg";
+
+    @Autowired
+    public void setService(ITypeService service) {
+        this.service = service;
+    }
+
+    /**
+     *
+     * 获取当前页的分类
+     * @param curPage 当前第几页
+     */
     @GetMapping("/{page}")
-    public String list(@PathVariable("page") int cur_page,
+    public String list(@PathVariable("page") int curPage,
                        HttpSession session,
                        Model model){
         Page<Type> page = new Page<>();
-        page.setCur_Page(cur_page);
+        page.setCur_Page(curPage);
         page.setPage_size(10);
         Page<Type> listType = service.listType(page);
         System.out.println(page.getPage_tot());
-        if(session.getAttribute("operation") != null){
-            if("update".equals(session.getAttribute("operation"))){
+        if(session.getAttribute(OPERATION) != null){
+            if(UPDATE.equals(session.getAttribute(OPERATION))){
                 model.addAttribute("message","恭喜，修改成功!");
-                session.removeAttribute("operation");
-            }else if("delete".equals(session.getAttribute("operation"))){
+                session.removeAttribute(OPERATION);
+            }else if(DELETE.equals(session.getAttribute(OPERATION))){
                 model.addAttribute("message","恭喜，删除成功!");
-                session.removeAttribute("operation");
+                session.removeAttribute(OPERATION);
             }else {
                 model.addAttribute("message","恭喜，添加成功!");
-                session.removeAttribute("operation");
+                session.removeAttribute(OPERATION);
             }
         }
         model.addAttribute("page",listType);
@@ -43,7 +76,10 @@ public class TypeController {
     }
 
 
-    //修改分类标签
+    /**
+     *
+     * 修改分类标签
+     */
     @PostMapping("/{id}")
     public String updateType(@PathVariable("id") Long id,
                              @RequestParam("name") String name,
@@ -62,7 +98,9 @@ public class TypeController {
     }
 
 
-    //跳转到修改分类标签的页面
+    /**
+     * 跳转到修改分类标签的页面
+     */
     @GetMapping("/input/{id}")
     public String input(@PathVariable("id") Long id,Model model){
         Type type = service.getType(id);
@@ -70,9 +108,12 @@ public class TypeController {
         return "admin/types-input";
     }
 
+    /**
+     * 跳转到新增分类的页面
+     */
     @GetMapping("/input")
     public String toAddInput(Model model,HttpSession session){
-        if(session.getAttribute("msg") != null){
+        if(session.getAttribute(MSG) != null){
             model.addAttribute("msg","请勿重复添加标签!");
             session.removeAttribute("msg");
         }
@@ -80,6 +121,10 @@ public class TypeController {
         return "admin/types-input";
     }
 
+    /**
+     *
+     * 新增分类
+     */
     @PostMapping("/addType")
     public String addInput(@RequestParam("name") String name,
                            HttpSession session){
